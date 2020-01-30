f2d923bede595e89d74f23e1c962ec088eeb7d87
francescolaspina <52795358+francescolaspina@users.noreply.github.com>
2020-01-30 15:24:19 +0100

    rinominato un service

--- src/main/java/net/bitsrl/progacademiaspringboot/controller/AcademyRestController.java
+++ src/main/java/net/bitsrl/progacademiaspringboot/controller/AcademyRestController.java
@@ -1,19 +1,18 @@
 package net.bitsrl.progacademiaspringboot.controller;
 
 import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbCourseAndCourseEditionService;
-import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbEnrollmentAndStudentService;
+import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AcademyRestController {
 
     private AbCourseAndCourseEditionService CourseAndCourseEditionService;
-    private AbEnrollmentAndStudentService EnrollmentAndStudentService;
+    private AbStudentService EnrollmentAndStudentService;
 
     @Autowired
-    public AcademyRestController(AbCourseAndCourseEditionService CourseAndCourseEditionService, AbEnrollmentAndStudentService EnrollmentAndStudentService) {
+    public AcademyRestController(AbCourseAndCourseEditionService CourseAndCourseEditionService, AbStudentService EnrollmentAndStudentService) {
         this.CourseAndCourseEditionService = CourseAndCourseEditionService;
         this.EnrollmentAndStudentService = EnrollmentAndStudentService;
     }
--- src/main/java/net/bitsrl/progacademiaspringboot/controller/StudentRestController.java
+++ src/main/java/net/bitsrl/progacademiaspringboot/controller/StudentRestController.java
@@ -2,45 +2,42 @@ package net.bitsrl.progacademiaspringboot.controller;
 
 import net.bitsrl.progacademiaspringboot.model.Student;
 import net.bitsrl.progacademiaspringboot.persistence.repositories.DataException;
-import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbCourseAndCourseEditionService;
-import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbEnrollmentAndStudentService;
+import net.bitsrl.progacademiaspringboot.persistence.services.abstractions.AbStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/student")
 public class StudentRestController {
-    private AbEnrollmentAndStudentService abEnrollmentAndStudentService;
+    private AbStudentService abStudentService;
 
     @Autowired
-    public StudentRestController(AbEnrollmentAndStudentService abEnrollmentAndStudentService) {
-        this.abEnrollmentAndStudentService = abEnrollmentAndStudentService;
+    public StudentRestController(AbStudentService abStudentService) {
+        this.abStudentService = abStudentService;
     }
 
     @GetMapping("/getall")
     public List<Student> getAllStudents() throws DataException {
-        List<Student> students = abEnrollmentAndStudentService.studentsGetAll();
+        List<Student> students = abStudentService.studentsGetAll();
         return students;
     }
 
     @PostMapping("/create")
     public Student createStudent(Student student) throws DataException {
-        abEnrollmentAndStudentService.studentCreateOrUpdate(student);
+        abStudentService.studentCreateOrUpdate(student);
         return student;
     }
 
     @PostMapping("/update")
     public Student updateStudent(Integer idStudent, Student student){
-        abEnrollmentAndStudentService.studentCreateOrUpdate(student);
+        abStudentService.studentCreateOrUpdate(student);
         return student;
     }
 
     @GetMapping("/delete/{id}")
     public void deleteStudent(@PathVariable Integer id){
-        abEnrollmentAndStudentService.studentDelete(id);
+        abStudentService.studentDelete(id);
     }
 }
